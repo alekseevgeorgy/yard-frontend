4c0a503066e1b3df636f2e1713c3d190ee391e1e
Georgy Alekseev <alekseevgeorgy@gmail.com>
2017-06-26 23:57:22 +0500

    fix eslint errors in scrollToTopRoute.jsx

--- src/ScrollToTopRoute.jsx
+++ src/ScrollToTopRoute.jsx
@@ -1,4 +1,3 @@
-/* eslint no-shadow: "off" */
 import React, { Component } from 'react';
 import { Route, withRouter } from 'react-router-dom';
 import './App.css';
@@ -14,9 +13,9 @@ class ScrollToTopRoute extends Component {
   }
 
   render() {
-    const { component: Component, ...rest } = this.props;
+    const { component: ExtractedComponent, ...rest } = this.props;
 
-    return <Route {...rest} render={props => <Component {...props} />} />;
+    return <Route {...rest} render={props => <ExtractedComponent {...props} />} />;
   }
 }
 
